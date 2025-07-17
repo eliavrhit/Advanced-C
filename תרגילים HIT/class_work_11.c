84198449e191bdb941126e49c7ae2e1bc490a9ea
eliav <eliavr@hit.ac.il>
2025-07-17 17:37:09 +0300

    .

---
+++ תרגילים HIT/class_work_11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main() {
+
+    FILE* file = fopen()
+}
+
+int countLines (char* filename) {
+
+    int numLines;
+    numLines=countLines("input.txt");
+    printf("The number of lines is %d", numLines);
+}
+
+// void letterFrequency (char* filename, int counts[])
+// {
+//
+// }
+//
+//
+// void removeSpaces (char* infile, char* outfile)
+// {
+//
+// }
